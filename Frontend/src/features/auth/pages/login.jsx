1dd9e81717e4c5bb22bf42b0d1a6b1123e1eb48e
Mrunal Jagtap <mrunaljagtap395@gmail.com>
2026-09-16 22:06:22 +0530

    auth changes

--- Frontend/src/features/auth/pages/login.jsx
+++ Frontend/src/features/auth/pages/login.jsx
@@ -1,16 +1,31 @@
+import "../pages/auth.form.scss"
+import {Link} from "react-router"
+
 const Login = () => {
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+  }
+
   return (
     <main>
-      <h1>Login</h1>
-      <form>
-        <label htmlFor="email">Email</label>
-        <input id="email" name="email" type="email" required />
+      <div className="form-container">
+        <h1>Login</h1>
+        <form onSubmit={(e)=>handleSubmit(e)}>
+          <div className="input-group">
+            
+            <label htmlFor="email">Email</label>
+            <input id="email" name="email" type="email" required />
+          </div>
 
-        <label htmlFor="password">Password</label>
-        <input id="password" name="password" type="password" required />
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input id="password" name="password" type="password" required />
+          </div>
 
-        <button type="submit">Login</button>
-      </form>
+          <button className="button primary-button" >Login</button>
+        </form>
+        <p>Didn't have an account ? <Link to={"/register"}>Register here !!</Link> </p>
+      </div>
     </main>
   )
 }
--- Frontend/src/features/auth/pages/register.jsx
+++ Frontend/src/features/auth/pages/register.jsx
@@ -1,19 +1,36 @@
+import "../pages/auth.form.scss"
+import { Link, Navigate } from "react-router"
+
+
+
 const Register = () => {
+
+    // const navigate=Navigate();
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        // navigate("/");
+        
+    }
+
     return (
         <main>
-            <h1>Register</h1>
-            <form>
-                <label htmlFor="name">Name</label>
-                <input id="name" name="name" type="text" required />
+            <div className="form-container">
+                <h1>Register</h1>
+                <form onSubmit={(e)=>handleSubmit(e)}>
+                    <label htmlFor="name">Name</label>
+                    <input id="name" name="name" type="text" required />
 
-                <label htmlFor="email">Email</label>
-                <input id="email" name="email" type="email" required />
+                    <label htmlFor="email">Email</label>
+                    <input id="email" name="email" type="email" required />
 
-                <label htmlFor="password">Password</label>
-                <input id="password" name="password" type="password" required />
+                    <label htmlFor="password">Password</label>
+                    <input id="password" name="password" type="password" required />
 
-                <button type="submit">Register</button>
-            </form>
+                    <button className="button primary-button" >Register</button>
+                </form>
+                <p>Already Registered ? <Link to={"/login"}>Login</Link></p>
+            </div>
         </main>
     )
 }
