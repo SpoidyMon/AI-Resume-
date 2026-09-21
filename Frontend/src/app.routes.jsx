35c8af0408c94026be7bfbe810817fa8972381c6
Mrunal Jagtap <mrunaljagtap395@gmail.com>
2026-09-21 17:08:33 +0530

    Home Page

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router'
 import Login from './features/auth/pages/login.jsx'
 import Register from './features/auth/pages/register.jsx'
 import Protected from './features/auth/components/protected.jsx'
+import Home from './features/Interview/pages/Home.jsx'
 
 export const router = createBrowserRouter([
 	{
@@ -14,6 +15,6 @@ export const router = createBrowserRouter([
 	},
 	{
 		path:'/',
-		element:<Protected><h1>Home Page</h1></Protected>
+		element:<Protected><Home /></Protected>
 	}
 ])
